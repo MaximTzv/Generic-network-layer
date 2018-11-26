85b827e5b6f633f7ea4f68fd06dba3625e8b297a
Maksim Tsvetkov <maksim.ts@apples-macbook-air.local>
2018-11-26 12:16:20 +0200

    Initial Commit

---
+++ Enceladus 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Enceladus 2.0
+//
+//  Created by Maksim Tsvetkov on 11/26/18.
+//  Copyright © 2018 Maksim Tsvetkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
